ad37acc70445aaa230074da89bec411ffbe23804
Theis <theismejnertsen@gmail.com>
2025-03-22 12:00:17 +0100

    [p1] updated zephyr platform to use interrupt driver uart

--- app/modules/dsmr_p1/src/zephyr/platform.c
+++ app/modules/dsmr_p1/src/zephyr/platform.c
@@ -15,6 +15,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/sys/ring_buffer.h>
 
 /******************************************************************************
  * Constants
@@ -32,6 +33,7 @@ const struct gpio_dt_spec data_req_gpio = GPIO_DT_SPEC_GET_OR(DT_NODELABEL(zephy
  * Local Function Prototypes
  *****************************************************************************/
 
+static void uart_irq_cb(const struct device *uart, void *user_data);
 static void thread_entry(void *p1, void *p2, void *p3);
 static int log_translate(platform_log_level_t log_level);
 
@@ -40,12 +42,15 @@ static int log_translate(platform_log_level_t log_level);
  * Local Variables
  *****************************************************************************/
 
-static uint8_t rx_buf[CONFIG_DSMR_P1_BUF_SIZE] = {0};
-
 static struct k_thread dsmr_p1_rx_thread;
-K_THREAD_STACK_DEFINE(dsmr_p1_rx_stack, CONFIG_DSMR_P1_BUF_SIZE * 2);
+K_THREAD_STACK_DEFINE(dsmr_p1_rx_stack, DSMR_P1_TELEGRAM_MAX_SIZE * 2);
 
 static data_received_callback_t telegram_received_cb;
+
+static uint8_t rx_buf[DSMR_P1_TELEGRAM_MAX_SIZE];
+static int rx_offset = 0;
+RING_BUF_DECLARE(rx_ring_buf, DSMR_P1_TELEGRAM_MAX_SIZE *2);
+K_SEM_DEFINE(data_ready_sem, 0, 1)
 
 
 /******************************************************************************
@@ -65,13 +70,11 @@ int platform_init(data_received_callback_t cb) {
             LOG_ERR("could not configure data request gpio: %d", ret);
             return ret;
         }
-
-
-        ret = gpio_pin_set_dt(&data_req_gpio, 1);
-        if (ret < 0) {
-            LOG_ERR("could not set data request: %d", ret);
-            return ret;
-        }
+    }
+	uart_irq_rx_disable(p1_uart_dev);
+    ret = uart_irq_callback_user_data_set(p1_uart_dev, uart_irq_cb, NULL);
+    if (ret < 0) {
+        return ret;
     }
 
     telegram_received_cb = cb;
@@ -81,6 +84,8 @@ int platform_init(data_received_callback_t cb) {
 
 int platform_write_data_req(bool high) {
     int ret = 0;
+
+	high ? uart_irq_rx_enable(p1_uart_dev) : uart_irq_rx_disable(p1_uart_dev);
     if (gpio_is_ready_dt(&data_req_gpio)) {
         ret = gpio_pin_set_dt(&data_req_gpio, high ? 1 : 0);
         if (ret < 0) {
@@ -121,48 +126,60 @@ SYS_INIT(dsmr_p1_init, POST_KERNEL, 90);
  * Local Function Implementation
  *****************************************************************************/
 
+static void uart_irq_cb(const struct device *uart_dev, void *user_data) {
+ 	if (!uart_irq_update(uart_dev)) {
+		LOG_DBG("Unable to process interrupts");
+		return;
+	}
+
+	if (!uart_irq_rx_ready(uart_dev)) {
+		LOG_DBG("No RX data");
+		return;
+	}
+    
+    int ret = uart_fifo_read(uart_dev, rx_buf + rx_offset, 1);
+    if (ret < 0) {
+        LOG_ERR("Failed to read UART FIFO (%d)", ret);
+        rx_offset = 0;
+        return;
+    };
+    if (rx_buf[0] != '/') {
+        rx_offset = 0;
+        return;
+    }
+
+    rx_offset += ret;
+    if (rx_offset < DSMR_P1_TRAILER_LEN) {
+        return; 
+    }
+    if (rx_offset >= sizeof(rx_buf)) {
+        rx_offset = 0;
+    }
+    if (rx_buf[rx_offset - DSMR_P1_TRAILER_LEN] == '!') {
+        ring_buf_put(&rx_ring_buf, rx_buf, rx_offset);
+        k_sem_give(&data_ready_sem);
+        rx_offset = 0;
+    }
+}
+
 static void thread_entry(void *p1, void *p2, void *p3) {
     int ret;
-    bool is_crc = true;
-    int crc_count = 0;
-    uint16_t crc = 0;
+    uint8_t buf[DSMR_P1_TELEGRAM_MAX_SIZE];
     LOG_INF("started");
 
-    int offset = 0;
-    for (;;) { // TODO: add timeout
-        if (offset >= sizeof(rx_buf)){
-            LOG_HEXDUMP_WRN(rx_buf, offset, "buffer overflow");
-            goto reset;
-        }
-        ret = uart_poll_in(p1_uart_dev, &rx_buf[offset]);        
+    for (;;) {
+        memset(buf, 0, sizeof(buf));
+        ret = k_sem_take(&data_ready_sem, K_FOREVER);
         if (ret != 0) {
             continue;
         }
-
-        if (!is_crc) {
-            if (rx_buf[0] != '/') {
-                goto reset;
-            }
-            if (rx_buf[offset] == '!') {
-                is_crc = true;
-                crc_count = 0;
-                crc = 0;
-            }
-        } else {
-            crc_count++;
-            if (crc_count == 4) {
-                telegram_received_cb(rx_buf, offset+1);
-                goto reset;
-            }
+        ret = ring_buf_get(&rx_ring_buf, buf, sizeof(buf));
+        if (ret < 0) {
+            LOG_ERR("could not get data from ring buf (%d)", ret);
+            continue;
         }
-        offset++;
-        continue;
-        
-reset:
-        offset = 0;
-        is_crc = false;
-        crc = 0;
-
+        LOG_HEXDUMP_INF(buf, ret, "telegram: ");
+        telegram_received_cb(buf, ret);
     }
 }
 
